354f3bce53a2d235977fd8ab762dedc7c8fa2457
kx30 <nikolay199808@gmail.com>
2018-10-03 10:23:57 +0300

    Create navigation view

--- app/src/main/java/com/example/nikolay/testsapplication/MainActivity.java
+++ app/src/main/java/com/example/nikolay/testsapplication/MainActivity.java
@@ -11,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); 
-
+        setContentView(R.layout.activity_main);
         initToolbar();
     }
 
